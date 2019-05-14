e22920993cb9311a98cfe73c3ba320795a876bf2
Deshun <admin@w3h5.com>
2019-05-14 22:14:31 +0800

    描述信息

---
+++ welcome.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Welcome <?php echo $_SERVER['HTTP_HOST']; ?> 欢迎访问德顺下载站</title>
+    <link rel="stylesheet" href="css/mustard-ui.min.css">
+    <link rel="stylesheet" href="css/base.css">
+</head>
+<body>
+<div class="row">
+    <div class="col col-lg-8">
+        <h5>说明</h5>
+        <p>欢迎来到德顺网络下载站欢迎页，网站暂时不提供检索。</p>
+        <p>如果您需要查找资源，请访问我的网站：</p>
+        <ul>
+            <li>德顺博客：<a href="http://www.dblog.cc">www.dblog.cc</a></li>
+            <li>前端资源网：<a href="https://www.w3h5.com">www.w3h5.com</a></li>
+            <li>德顺秒赞网：<a href="http://www.iqqmz.com">www.iqqmz.com</a></li>
+            <li>德顺网络论坛：<a href="http://bbs.52deshun.com">bbs.52deshun.com</a></li>
+        </ul>
+    </div>
+</div>
+<div class="row">
+    <div class="col col-lg-8">
+        <p>欢迎关注我的社交媒体：</p>
+        <ul>
+            <li>微博：<a target="_blank" href="https://weibo.com/2493372337">@无码表情包</a></li>
+            <li>Twitter：<a target="_blank" href="https://twitter.com/AlasLiu">@AlasLiu</a></li>
+            <li>GitHub：<a target="_blank" href="https://github.com/ideshun">@ideshun</a></li>
+            <li>百家号：<a target="_blank" href="https://baijiahao.baidu.com/u?app_id=1582878168111795">德顺哥</a></li>
+            <li>联系QQ：<a href="tencent://Message/?Uin=1209278955">1209278955</a></li>
+            <li>前端交流群：<a href="https://jq.qq.com/?_wv=1027&k=5fIUoFe">133413450</a></li>
+            <li>QQ公众空间：<a href="https://user.qzone.qq.com/203666169">德顺传媒</a></li>
+            <li>扫一扫下方二维码关注我的微信&公众号</li>
+            <p>
+                <img src="img/WX_QRCode.jpg" alt="">
+                <img src="img/MP_WX_QRCode.jpg" alt="">
+            </p>
+        </ul>
+        <p>
+            <small>&copy;<?php echo date("Y")?> By Deshun</small>
+        </p>
+    </div>
+</div>
+</body>
+</html>
